c67ced1efd04a2613ff2c7c9c603f43194852761
vlooL <52248468+vlooL7@users.noreply.github.com>
2024-04-08 19:15:50 +0300

    chore: add alias baseUrl "./src"

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { AliasOptions, defineConfig } from 'vite'
+
+const alias: AliasOptions = {}
+
+const aliasPaths = ['pages', 'widgets', 'features', 'entities', 'shared']
+
+aliasPaths.forEach(aliasPath => {
+	alias[aliasPath] = path.resolve(__dirname, 'src', aliasPath)
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	resolve: { alias }
 })
