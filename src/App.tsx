516bdf20e3980d664b3a743da8a5b46ee06060bf
vlooL <52248468+vlooL7@users.noreply.github.com>
2024-04-10 01:25:45 +0300

    chore: add react-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { TodosColumnsPage } from 'pages/todos-columns'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 export default function App() {
-	return <TodosColumnsPage />
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<TodosColumnsPage />
+		</DndProvider>
+	)
 }
