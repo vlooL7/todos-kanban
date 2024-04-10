9d64725b847143ed2a842cdeeda7783144fa6bcf
vlooL <52248468+vlooL7@users.noreply.github.com>
2024-04-10 20:03:22 +0300

    feat(widgets): add dnd for todos-column

---
+++ src/widgets/todos-column/hooks/useTodosColumnDnD.ts
@@ -0,0 +1,76 @@
+import { useUnit } from 'effector-react'
+import { TodosColumn, todosColumnsModel } from 'entities/todos-columns'
+import { useDrag, useDrop } from 'react-dnd'
+import {
+	DnDTypes,
+	TodoInTodosColumnDnDItem,
+	TodosColumnDnDItem,
+	useLatest
+} from 'shared/lib'
+
+export const useTodosColumnDnD = (todosColumn: TodosColumn) => {
+	const todosColumnsApi = useUnit(todosColumnsModel.todosColumnsApi)
+	const todoInTodosColumnApi = useUnit(todosColumnsModel.todoInTodosColumnApi)
+
+	const latest = useLatest({
+		todosLength: todosColumn.todos.length
+	})
+	const todosColumnItemLatest = useLatest<TodosColumnDnDItem>({
+		id: todosColumn.id
+	})
+
+	const [{ isDragging }, drag, dragPreview] = useDrag(
+		() => ({
+			type: DnDTypes.TODOS_COLUMN,
+			item: todosColumnItemLatest.current satisfies TodosColumnDnDItem,
+			collect: monitor => ({
+				isDragging: monitor.isDragging()
+			})
+		}),
+		[todosColumnItemLatest]
+	)
+
+	const [{ isOver }, drop] = useDrop(
+		() => ({
+			accept: DnDTypes.TODOS_COLUMN,
+			hover: (item: TodosColumnDnDItem) => {
+				const todosColumnItem = todosColumnItemLatest.current
+
+				todosColumnsApi.moveAfter({
+					todosColumn: item,
+					todosColumnAfter: todosColumnItem
+				})
+			},
+			collect: monitor => ({
+				isOver: !!monitor.isOver()
+			})
+		}),
+		[todosColumnItemLatest, todosColumnsApi]
+	)
+
+	const [{ isTodoOver }, dropTodo] = useDrop(
+		() => ({
+			accept: DnDTypes.TODO_IN_TODOS_COLUMN,
+			hover: (item: TodoInTodosColumnDnDItem) => {
+				if (latest.current.todosLength !== 0) return
+
+				const todosColumnItem = todosColumnItemLatest.current
+				if (todosColumnItem.id === item.todosColumnId) return
+
+				todoInTodosColumnApi.pushFromTo({
+					todoFromId: item.id,
+					todosColumnFromId: item.todosColumnId,
+					todosColumnToId: todosColumnItem.id
+				})
+
+				item.todosColumnId = todosColumnItem.id
+			},
+			collect: monitor => ({
+				isTodoOver: !!monitor.isOver()
+			})
+		}),
+		[latest, todoInTodosColumnApi, todosColumnItemLatest]
+	)
+
+	return { isDragging, isOver, isTodoOver, drag, dragPreview, drop, dropTodo }
+}
--- src/widgets/todos-column/ui/TodosColumn.tsx
+++ src/widgets/todos-column/ui/TodosColumn.tsx
@@ -1,27 +1,52 @@
-import { Todo } from 'widgets/todo'
 import { todosColumnsModel } from 'entities/todos-columns'
-import { memo } from 'react'
+import { Grip } from 'lucide-react'
+import { memo, useLayoutEffect, useRef } from 'react'
 import { Card, CardTitle } from 'shared/components'
+import { twMerge } from 'tailwind-merge'
+import { Todo } from 'widgets/todo'
+import { useTodosColumnDnD } from '../hooks'
 import { TodosColumnDropdown } from './TodosColumnDropdown'
 
 export type TodosColumnProps = {
 	todosColumn: todosColumnsModel.TodosColumn
 }
 const TodosColumnView = ({ todosColumn }: TodosColumnProps) => {
+	const ref = useRef<HTMLDivElement>(null)
+
+	const { isDragging, isOver, isTodoOver, drag, dragPreview, drop, dropTodo } =
+		useTodosColumnDnD(todosColumn)
+
+	useLayoutEffect(() => {
+		dropTodo(drop(dragPreview(ref)))
+	}, [dragPreview, drop, dropTodo])
+
 	return (
-		<Card className="w-[300px] p-4">
-			<CardTitle className="flex text-md pb-2 justify-between">
-				{todosColumn.title}
-				<TodosColumnDropdown todosColumn={todosColumn} />
-			</CardTitle>
-			<div className="flex flex-col gap-2">
-				{todosColumn.todos.map(todo => {
-					return (
-						<Todo key={todo.id} todosColumnId={todosColumn.id} todo={todo} />
-					)
-				})}
-			</div>
-		</Card>
+		<div ref={ref}>
+			<Card
+				className={twMerge(
+					'w-[300px] p-4',
+					isDragging && 'opacity-50',
+					isOver && 'bg-neutral-100',
+					isTodoOver && 'border-sky-500'
+				)}>
+				<CardTitle className="flex text-md pb-2 justify-between items-center">
+					<div className="flex justify-end items-center gap-2">
+						<div ref={drag} role="handle">
+							<Grip className="cursor-move" size="1em" />
+						</div>
+						{todosColumn.title}
+					</div>
+					<TodosColumnDropdown todosColumn={todosColumn} />
+				</CardTitle>
+				<div className="flex flex-col gap-2">
+					{todosColumn.todos.map(todo => {
+						return (
+							<Todo key={todo.id} todosColumnId={todosColumn.id} todo={todo} />
+						)
+					})}
+				</div>
+			</Card>
+		</div>
 	)
 }
 
