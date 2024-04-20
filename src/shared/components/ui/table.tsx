5eb9990a9479815acfa0156593d192c2965fc3e4
vlooL <52248468+vlooL7@users.noreply.github.com>
2024-04-20 19:57:07 +0300

    feat(app): add ScrollArea

--- src/shared/components/ui/table.tsx
+++ src/shared/components/ui/table.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react'
 
 import { cn } from 'shared/lib/ui'
+import { ScrollArea } from './scroll-area'
 
 const Table = React.forwardRef<
 	HTMLTableElement,
 	React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-	<div className="relative w-full overflow-auto">
+	<ScrollArea className="relative w-full">
 		<table
 			ref={ref}
 			className={cn('w-full caption-bottom text-sm', className)}
 			{...props}
 		/>
-	</div>
+	</ScrollArea>
 ))
 Table.displayName = 'Table'
 
--- src/widgets/todos-columns/ui/TodosColumns.tsx
+++ src/widgets/todos-columns/ui/TodosColumns.tsx
@@ -1,17 +1,18 @@
 import { useUnit } from 'effector-react'
 import { todosColumnsModel } from 'entities/todos-columns'
+import { ScrollArea } from 'shared/components'
 import { TodosColumn } from 'widgets/todos-column'
 
 export const TodosColumns = () => {
 	const todosColumns = useUnit(todosColumnsModel.$todosColumns)
 
 	return (
-		<div className="flex flex-1 overflow-auto">
-			<div className="flex flex-row items-start gap-4 px-8 py-4 h-max mx-auto">
+		<ScrollArea className="flex flex-1">
+			<div className="flex flex-row items-start gap-4 px-8 py-4 h-max w-min mx-auto">
 				{todosColumns.map(todoColumn => {
 					return <TodosColumn key={todoColumn.id} todosColumn={todoColumn} />
 				})}
 			</div>
-		</div>
+		</ScrollArea>
 	)
 }
