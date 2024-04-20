1641cc1ddf73ad8a152a40ad84e7c3765f78b6b5
vlooL <52248468+vlooL7@users.noreply.github.com>
2024-04-20 17:29:24 +0300

    feat(todos-list): remake to table

--- src/widgets/todos-list/ui/TodosList.tsx
+++ src/widgets/todos-list/ui/TodosList.tsx
@@ -1,20 +1,37 @@
 import { useUnit } from 'effector-react'
 import { todosModel } from 'entities/todos'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from 'shared/components'
 
 export const TodosList = () => {
 	const todos = useUnit(todosModel.$todos)
 
 	return (
-		<div className="flex overflow-auto">
-			<div className="flex flex-col gap-4 px-8 py-4 h-max">
-				{todos.map(todo => {
-					return (
-						<p key={todo.id}>
-							{todo.title} {new Date(todo.created_at).toDateString()}
-						</p>
-					)
-				})}
-			</div>
-		</div>
+		<Table className="h-max w-full">
+			<TableHeader>
+				<TableRow>
+					<TableHead>Todo</TableHead>
+					<TableHead>Created</TableHead>
+					<TableHead>Method</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				{todos.map(todo => (
+					<TableRow key={todo.id}>
+						<TableCell className="font-medium">{todo.title}</TableCell>
+						<TableCell className="text-nowrap">
+							{new Date(todo.created_at).toDateString()}
+						</TableCell>
+						<TableCell>{todo.description}</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
 	)
 }
